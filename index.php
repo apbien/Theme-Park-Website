c2629e6127c96bef97d8a6a21024014397eb8f14
apbien <49501701+apbien@users.noreply.github.com>
2019-04-10 19:13:32 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Themepark</title>
+		<link rel="stylesheet" type="text/css" href="stylesheets/style_navigation.css"/>
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="banner">
+
+				<div class="menubuttons">
+					<ul>
+						<li><a href="apply.php">APPLY</a> </li>
+						<li><a href="login.php">LOGIN </a> </li>
+						<li><a href="register.php">REGISTER </a> </li>
+					</ul>
+				</div>
+				<div class = "parkname">
+					SKYLAND
+				</div>
+
+			</div>
+
+			<nav id="navigation">
+				<ul id="nav">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="#">Shops</a></li>
+					<li><a href="#">Rides</a></li>
+					<li><a href="#">Events</a></li>
+					<li><a href="#">Tickets</a></li>
+				</ul>
+			</nav>
+		
+		</div>
+
+	</body>
+</html>
+
